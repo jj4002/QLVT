68e43918c9da05e335b1babe84e2e9224bd7ed58
jj4002 <svptithcm2k4@gmail.com>
2025-04-17 19:52:47 +0700

    sửa lại fmMain.cs chút

--- QLVT5/fmMain.cs
+++ QLVT5/fmMain.cs
@@ -9,29 +9,27 @@ using System.Windows.Forms;
 
 namespace QLVT5
 {
-    private Form CheckExists(Type ftype)
-    {
-        foreach (Form f in this.MdiChildren)
-            if (f.GetType() == ftype)
-                return f;
-        return null;
-    }
-
-    private void btnNhanvien_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
-    {
-        Form frm = this.CheckExists(typeof(frmNhanvien));
-        if (frm != null) frm.Activate();
-        else
-        {
-            frmNhanvien f = new frmNhanvien();
-            f.MdiParent = this;
-            f.Show();
-        }
-    }
-
-
     public partial class fmMain : DevExpress.XtraBars.Ribbon.RibbonForm
     {
+       private Form CheckExists(Type ftype)
+        {
+            foreach (Form f in this.MdiChildren)
+                if (f.GetType() == ftype)
+                    return f;
+            return null;
+        }
+
+        //private void btnNhanvien_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        //{
+        //    Form frm = this.CheckExists(typeof(frmNhanvien));
+        //    if (frm != null) frm.Activate();
+        //    else
+        //    {
+        //        frmNhanvien f = new frmNhanvien();
+        //        f.MdiParent = this;
+        //        f.Show();
+        //    }
+        //}
         public fmMain()
         {
             InitializeComponent();
